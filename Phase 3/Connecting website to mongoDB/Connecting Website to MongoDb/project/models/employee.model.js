492e3ffd1b07c1a9576abf9d3e775147863c4b64
Ptp26 <71855186+Ptp26@users.noreply.github.com>
2021-09-07 16:37:40 -0400

    Website connection to mongodB

---
+++ Phase 3/Connecting website to mongoDB/Connecting Website to MongoDb/project/models/employee.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+var employeeSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    deptId: {
+        type: String
+    }
+});
+
+
+mongoose.model('Employee', employeeSchema);
